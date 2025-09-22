81a0d589e5cd001e8ad32b5566d3dc08cbced79f
SRAS2024 <samuelrasimonds@gmail.com>
2025-09-21 22:25:41 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const session = require("express-session");
 const path = require("path");
-const { verifyJwt } = require("./final_project/middleware/auth");
-const customer_routes = require("./final_project/router/auth_users").authenticated;
-const genl_routes = require("./final_project/router/general").general;
+
+// Adjusted paths because index.js is now at root
+const { verifyJwt } = require("./middleware/auth");
+const customer_routes = require("./router/auth_users").authenticated;
+const genl_routes = require("./router/general").general;
 
 const app = express();
 
@@ -20,8 +22,8 @@ app.use(
   })
 );
 
-// Static client
-app.use(express.static(path.join(__dirname, "final_project/client")));
+// Serve static files from client folder
+app.use(express.static(path.join(__dirname, "client")));
 
 // Protect all customer auth routes
 app.use("/customer/auth/*", verifyJwt);
